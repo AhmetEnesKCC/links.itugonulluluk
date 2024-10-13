58295313dfb2cce494ceebfa02756b68265de29c
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2024-10-13 23:58:02 +0300

    ui fix

--- src/components/label-icon-button.tsx
+++ src/components/label-icon-button.tsx
@@ -7,9 +7,9 @@ const LabelIconButton = (props: LabelIconButtonProps) => {
     <Link href={props.link} target="_blank">
       <Button variant={"outline"} className="min-h-[48px] w-full">
         <div className="justify-between  items-center flex w-full h-full gap-x-4">
-          <span className="text-ellipsis overflow-hidden flex-1 text-left">
+          <span className="text-ellipsis overflow-hidden  w-full">
             {props.label}
-          </span>{" "}
+          </span>
           <div className="h-full">{props.icon}</div>
         </div>
       </Button>
