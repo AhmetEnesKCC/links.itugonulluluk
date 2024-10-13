a8462587079490d45ec43fcfa916be322cf26b9a
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2024-10-13 17:45:49 +0300

    fixed links

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,44 +1,42 @@
 import { IconButtonProps } from "@/types/home";
 import { Button } from "./ui/button";
-import Link from "next/link";
 import React from "react";
 import { tailwindColors } from "@/constants/tailwind-config";
 import clsx from "clsx";
 import { Badge } from "./ui/badge";
+import Link from "next/link";
 
 const IconLink = (props: IconButtonProps) => {
   return (
-    <Button
-      style={{
-        pointerEvents: props.soon ? "none" : "unset",
+    <Link href={props.link} target="_blank">
+      <Button
+        style={{
+          pointerEvents: props.soon ? "none" : "unset",
 
-        backgroundColor: props.soon
-          ? tailwindColors.foreground
-          : tailwindColors.background,
-      }}
-      variant={"outline"}
-      className={clsx(
-        "min-w-[48px] min-h-[48px] rounded-md relative aspect-square flex items-center justify-center",
-        !props.soon ? "hover:!bg-accent" : "hover:!bg-accent-foreground"
-      )}
-    >
-      <Link
-        className="flex-1 self-stretch aspect-square  "
-        href={props.link}
-        target="_blank"
+          backgroundColor: props.soon
+            ? tailwindColors.foreground
+            : tailwindColors.background,
+        }}
+        variant={"outline"}
+        className={clsx(
+          "min-w-[48px] min-h-[48px] rounded-md relative aspect-square p-2 ",
+          !props.soon ? "hover:!bg-accent" : "hover:!bg-accent-foreground"
+        )}
       >
         {React.cloneElement(props.icon, {
           color: props.soon
             ? tailwindColors.background
             : tailwindColors.foreground,
         })}
-        {props.soon && (
-          <Badge variant={"secondary"} className="mt-1">
-            <span className="text-xs">Yakında</span>
-          </Badge>
-        )}
-      </Link>
-    </Button>
+        <div className="absolute bottom-[-18px]">
+          {props.soon && (
+            <Badge variant={"secondary"} className="">
+              <span className="text-[10px]">Yakında</span>
+            </Badge>
+          )}
+        </div>
+      </Button>
+    </Link>
   );
 };
 
--- src/data/icon-links.data.ts
+++ src/data/icon-links.data.ts
@@ -15,6 +15,11 @@ export const iconLinks: IconButtonProps[] = [
     icon: SI_ICONS.SiYoutube(commonProps),
     link: "https://www.youtube.com/@itugonulluluk",
   },
+
+  {
+    icon: SI_ICONS.SiLinkedin(commonProps),
+    link: "https://www.linkedin.com/company/it%C3%BC-g%C3%B6n%C3%BCll%C3%BCl%C3%BCk-kul%C3%BCb%C3%BC/",
+  },
   {
     icon: SI_ICONS.SiAppstore(commonProps),
     link: "",
@@ -24,9 +29,5 @@ export const iconLinks: IconButtonProps[] = [
     icon: SI_ICONS.SiGoogleplay(commonProps),
     link: "",
     soon: true,
-  },
-  {
-    icon: SI_ICONS.SiLinkedin(commonProps),
-    link: "https://www.linkedin.com/company/it%C3%BC-g%C3%B6n%C3%BCll%C3%BCl%C3%BCk-kul%C3%BCb%C3%BC/",
   },
 ];
